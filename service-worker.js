0d83ba91b27d1eb1944fa47bac032f3d586832e5
laffumicato-cyber <l.affumicato@gmail.com>
2026-07-16 19:50:20 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE = 'microciclo-mtb-v1';
+const APP_FILES = [
+  './',
+  './microciclo_mtb_mobile-1.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      return response;
+    }).catch(() => caches.match('./microciclo_mtb_mobile-1.html')))
+  );
+});
